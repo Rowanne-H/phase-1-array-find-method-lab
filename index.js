008bdc38702835be8984f8bdb5772c1ad8b62bcb
YUERONG HUANG <rowanne.h@hotmail.com>
2024-01-15 21:29:05 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(arr) {
+    let year;
+    arr.find(obj => {
+        if (obj.result == 'W') {
+            return year = obj.year;
+        }
+    })
+    return year;
+}
